9e69dc4c6eb104dd60f4ea150e47fb07572ff427
zoesalinas <zoesalinas@najdorf.com>
2020-10-12 20:00:08 -0700

    finished tea maker

--- src/_04_tea_maker/TeaMakerRunner.java
+++ src/_04_tea_maker/TeaMakerRunner.java
@@ -1,5 +1,11 @@
 package _04_tea_maker;
 
 public class TeaMakerRunner {
-
+public static void main(String[] args) {
+	TeaBag teabag = new TeaBag("Mint");
+	Cup cup = new Cup();
+	Kettle kettle = new Kettle();
+	kettle.boil();
+	cup.makeTea(teabag, kettle.getWater());
+}
 }
